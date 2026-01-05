cd2c90e76c7ccc00c853f8ca7e9ad0344799158d
gaurav8228 <mandalgaurav350@gmail.com>
2026-01-05 20:02:56 +0530

    final subimssion8

--- dashboard/src/components/Home.js
+++ dashboard/src/components/Home.js
@@ -2,12 +2,15 @@ import React from "react";
 
 import Dashboard from "./Dashboard";
 import TopBar from "./TopBar";
-
+import ProtectedRoute from "./routes/ProtectedRoute";
 const Home = () => {
   return (
     <>
       <TopBar />
+       <ProtectedRoute>
       <Dashboard />
+    </ProtectedRoute>
+     
     </>
   );
 };
